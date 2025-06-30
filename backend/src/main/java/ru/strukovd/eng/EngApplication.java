f2cb5e3430893189e10aa1347af4207c16237305
strukovd <dmitryi97@yandex.ru>
2025-07-01 02:32:39 +0600

    moved backend init commit

---
+++ backend/src/main/java/ru/strukovd/eng/EngApplication.java
@@ -0,0 +1,13 @@
+package ru.strukovd.eng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/strukovd/eng/EngApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.strukovd.eng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

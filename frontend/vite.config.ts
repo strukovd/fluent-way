7cf16f5bb6e1f6260ef2b4b26ed18ad52d3e4ac7
strukovd <dmitryi97@yandex.ru>
2025-07-20 02:17:38 +0600

    added deploy.yml config and set Vite base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/fluent-way/',
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
